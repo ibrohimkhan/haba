cb52e17c69d10dd77818b8b0a17f41b8234410de
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2020-03-30 23:52:24 +0500

    configured app's colors, added view bindings, desgined ui in main activity layout

--- app/src/main/java/com/udacity/haba/ui/MainActivity.java
+++ app/src/main/java/com/udacity/haba/ui/MainActivity.java
@@ -1,16 +1,42 @@
 package com.udacity.haba.ui;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.udacity.haba.R;
+import com.udacity.haba.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.recipes:
+                    Toast.makeText(this, "Recipes selected", Toast.LENGTH_SHORT).show();
+                    return true;
+
+                case R.id.ingredients:
+                    Toast.makeText(this, "Ingredients selected", Toast.LENGTH_SHORT).show();
+                    return true;
+
+                case R.id.favorites:
+                    Toast.makeText(this, "Favorites selected", Toast.LENGTH_SHORT).show();
+                    return true;
+            }
+
+            return false;
+        });
+
+        binding.bottomNavigation.setSelectedItemId(R.id.ingredients);
     }
 }
