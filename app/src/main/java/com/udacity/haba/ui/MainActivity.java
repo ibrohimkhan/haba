a3d163aeb661375fc4bb10be8967aaf716d66a01
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2020-03-30 17:02:40 +0500

    project structure set up

---
+++ app/src/main/java/com/udacity/haba/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.udacity.haba.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.udacity.haba.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
